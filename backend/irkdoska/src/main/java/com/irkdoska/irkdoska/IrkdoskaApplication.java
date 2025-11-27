7ee0d9f895b1fa137b7bb2631aaeb92abf4b23f2
plaschinskij nikolaj <niccneimi@gmail.com>
2025-11-27 22:57:50 +0300

    feat: инициализация бэкенда

---
+++ backend/irkdoska/src/main/java/com/irkdoska/irkdoska/IrkdoskaApplication.java
@@ -0,0 +1,13 @@
+package com.irkdoska.irkdoska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrkdoskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrkdoskaApplication.class, args);
+	}
+
+}
---
+++ backend/irkdoska/src/test/java/com/irkdoska/irkdoska/IrkdoskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irkdoska.irkdoska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrkdoskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
